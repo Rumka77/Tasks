9c3940f32b5748cf428fe8207602fbcb93e25c38
Rumka77 <rumka1977@mail.ru>
2019-08-30 01:21:55 +0300

    almost ready

---
+++ src/DataService.js
@@ -0,0 +1,31 @@
+class  DataService {
+
+  static dateFormatted(data) {
+    return  data < 10 ? ("0"+data) : data;
+  }
+
+  static idToTimeSec(str) {
+    return parseInt(str.substr(8, 2))*60*60 +
+           parseInt(str.substr(10, 2))*60 +
+           parseInt(str.substr(12, 2));
+  }
+
+  static idToDateStr(str) {
+    return parseInt(str.substr(0, 8));
+  }
+
+  static dateNowToStr() {
+    return parseInt( this.dateFormatted( String( (new Date()).getDate() ) ) +
+                     this.dateFormatted( String( (new Date()).getMonth() +1) ) +
+                     this.dateFormatted( String( (new Date()).getFullYear() ) ) );
+  }
+
+  static timeSec() {
+    return ( (new Date()).getHours() )*60*60 +
+           ( (new Date()).getMinutes() )*60 +
+             (new Date()).getSeconds();
+  }
+
+}
+
+export default DataService;
